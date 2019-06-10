4dfe44fc628748f85f813effd904b394c12635e6
Darin Webster <darinleenavy@gmail.com>
2019-06-10 01:34:48 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,22 @@
+def #introduction(name)
+ puts "Hi, my name is #{name}."
+end
+
+#introduction(Mary)
+
+def #introduction_with_langauge(name, language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+ 
+ #introduction_with_langauge(Lisa, Python)
+ 
+ #introduction_with_langauge(Tom, React)
+ 
+ def #introduction_with_langauge_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+#introduction_with_langauge_optional(Becky, Ruby)
+
+#introduction_with_langauge_optional(Ron, Ruby)
+ 
