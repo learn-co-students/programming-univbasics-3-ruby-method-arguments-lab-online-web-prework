d989ef3b70a220b22d7a4b1cdefaebb1c9a659cc
Adam Schonely <aschonely@gmail.com>
2019-06-16 20:59:03 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language) do |name, "Ruby"|
+def introduction_with_language_optional(name, language = "Ruby") 
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end 
+end
