99cbc8c2c2a1865e8b107a3148f9756393bee51a
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-20 21:34:33 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,20 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+def introduction_with_language_optional(name, language = "Ruby")
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction("Josh")
+introduction("Dan")
+
+introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Josh", "React")
+
+introduction_with_language_optional("Edwin")
+introduction_with_language_optional("Edwin", "Python")
