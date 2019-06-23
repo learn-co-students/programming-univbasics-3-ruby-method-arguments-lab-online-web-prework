e7555ac93ec56125cce424b12f67ed4e14105038
Shahwar Spall <shevy.spall@gmail.com>
2019-06-23 03:49:36 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction (name)
+puts "Hi, my name is #{name}."
+end
+
+def introduction_language (name,language)
+  puts "my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional (name,language)
+  puts "my name is #{name} and I am learning to program in #{language}."
+end
