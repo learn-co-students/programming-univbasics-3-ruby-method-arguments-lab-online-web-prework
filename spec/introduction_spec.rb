10e2b2f359d3ab5460d74cbc76f223fd26930abf
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-27 22:06:13 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -21,7 +21,7 @@ describe "#introduction_with_language_optional" do
 end
 
 describe "#introduction_with_language_optional" do
-  it "takes in two arguments, a name and a language, language can be optional or defined" do
+  it "takes in two arguments, a name and a language, " do
     expect{introduction_with_language_optional("Edwin", "Python")}.to output("Hi, my name is Edwin and I am learning to program in Python.\n").to_stdout
   end
 end
