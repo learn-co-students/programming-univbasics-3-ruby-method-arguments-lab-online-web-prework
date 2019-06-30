77863de6a066b2b93c997de18e005c5496367eb4
Julie Cook <juliecook328@gmail.com>
2019-06-30 21:41:11 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+  #method name      #parameter
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+#introduction_with_language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
