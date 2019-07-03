70807a3acf7d823a3321888999bcd029d4d8cce6
Audrea Cook <audreacwork@gmail.com>
2019-07-03 18:51:14 +0000

    Done.

--- first_solution.rb
+++ first_solution.rb
@@ -1,4 +1,4 @@
-def introduction(name)
+def introduction(name) 
   puts "Hi, my name is #{name}."
 end
 
@@ -6,6 +6,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language = "Ruby")
+def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
