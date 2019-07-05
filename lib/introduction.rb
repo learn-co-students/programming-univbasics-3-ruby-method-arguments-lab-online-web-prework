53ef909a1b72526ddf31badd43005ad5f507c680
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-05 22:35:21 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,23 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("josh")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Calvin", "Ruby")
+
+
+def introduction_with_language_optional(name,
+  language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Josh")
+
+
+
+
