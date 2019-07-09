c1c318819bcceef0acfd008280bec7b8dfe1cd75
Deborah Savage <savaged725@yahoo.com>
2019-07-09 18:52:09 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def #introduction (name)
+  puts "Hi, my name is #{name}"
+end
+
+def #introduction_with_language (name, language)
+  puts "Hi, my name is #{name}"
+elseif "Hi, my name is #{name} and i am learning to program in #{language}"
+end
