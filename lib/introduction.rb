cd58e2c360d1bcd53f6e6947b19a0481bb0ce2c3
Matthew Van Eck <thematt84@yahoo.com>
2019-07-17 06:11:58 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,18 @@
+def introduction(name) 
+  puts "Hi, my name is #{name}."
+end
+name = "Matt"  
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+name = "Matt"
+language = "Ruby"
+
+
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+name = "Matt"
+
