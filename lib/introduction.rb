c100ec4533e5bada1690e5d3f4ddf4a81182df5c
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-17 07:02:46 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def introduction(Andrew)
+  puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language(Andrew, Ruby)
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional(Andrew, Ruby)
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}"
+end
+
+ 
