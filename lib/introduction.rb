022b3c0f51311d61a908e9b2e0eb14e9a8aa309d
Alfredo Diaz <mail4alfredo@gmail.com>
2019-07-26 22:48:48 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,15 @@
+def introduction(name = "Alfredo")
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name = "Alfredo", language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name = "Alfredo", language = "Python")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name = "Alfredo", language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
