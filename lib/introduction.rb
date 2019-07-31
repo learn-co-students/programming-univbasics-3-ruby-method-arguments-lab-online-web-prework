23920b65e7096d096ce0ff3de895f43dc98ae27d
Rachel T <rachmaybe@gmail.com>
2019-07-31 04:08:44 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+#introduction name, language
+def "introduction_language(name,language)"
+  puts "Hello, #{name}, and I am learning to program in #{language}"
+ end
+
+introduction_language ("Rachel", "Ruby")
+
