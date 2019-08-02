788c3ed87ebfc8bc28d511b609d1078ee026033e
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-02 23:09:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,4 @@
 #introduction_with_language_optional.
 it "takes in an argument of a name and puts out a phase with 
   that name using interpolation"
-  expect{introduction("joey")}. to output(Hi)
+  expect{introduction("joey")}. to output()
