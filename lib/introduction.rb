9dabfad2b7ec5cc1aab5a8c34bfd09fb88bea927
Kaitlyn Summerill <kaitlynsummerill@gmail.com>
2019-08-09 21:02:14 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,25 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end  
 
+introduction("Josh")
+introduction("Dan")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end  
+
+introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Josh", "React")
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end  
+
+introduction_with_language_optional("Edwin", "Python")
 
 
 
