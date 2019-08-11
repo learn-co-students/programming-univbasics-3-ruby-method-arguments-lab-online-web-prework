99c002cb909a8c7ed0d84c95324d621e00c0d64f
Lauren Dove <elledoveinc@gmail.com>
2019-08-11 01:21:41 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,16 @@
-def introduction(Boss)
+def introduction(name)
  puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(Iris , Javascript)
+def introduction_with_language(name , language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(Clemmy , language="Ruby" )
-  puts "Hi, my name is #{name} and I am learning to program in #{language/optional}."
+def introduction_with_language_optional(name , language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+
 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
