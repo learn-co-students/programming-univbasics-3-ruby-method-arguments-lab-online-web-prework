1a0e28bb1fc310bebc8a1be407305f90bd08d908
John Reyes <john.jreye@gmail.com>
2019-08-15 04:13:46 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,27 @@
 
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end  
+  introduction_with_language("Dan", "Ember.js")
+
+
+def introduction_with_language_optional(name, language ='Ruby')
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+  
+  introduction_with_language_optional("Edwin", "Ruby")
+  
+  introduction_with_language_optional("Edwin")
 
 
 
