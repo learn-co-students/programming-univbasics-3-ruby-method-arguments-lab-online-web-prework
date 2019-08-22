eea169c686776a649cb0c70900cb322d09114eb1
Michael Waldo <mbwcogic@yahoo.com>
2019-08-22 00:47:04 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,14 @@
+"#introduction" do
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
+    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+    expect{introduction("Dan")}.to output("Hi, my name is Dan.\n").to_stdout
+  end
+end
+
+
+
+
+
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
