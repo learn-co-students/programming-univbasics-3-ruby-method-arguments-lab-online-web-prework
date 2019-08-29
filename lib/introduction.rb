92e25a4bd03e40a3ca97dbc7020022138c4676bb
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-08-29 00:12:58 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,15 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def introduction_with_language_optional(name, language = "Ruby")
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
+
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
