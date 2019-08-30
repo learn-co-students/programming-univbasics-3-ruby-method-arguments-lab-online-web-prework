9910be7a494173a99ee2800830f06e98539100ce
Joshua Rodriguez <josh030596@gmail.com>
2019-08-30 12:30:09 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,22 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+name = "josh"
 
+def #introduction(name)
+puts "Hi, my name is #{name}.\n" 
+end 
 
+#introduction 
+
+def #introduction_with_language_optional(name, language = 'ruby')
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+#introduction_with_language_optional("josh") 
+
+def #introduction_with_language(name, language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+#introduction_with_language("josh", "elixir") 
 
 
 
