6274d4c5ab2a9972dcd63397169da3b0ccc65196
cerulean-installation-3033 <greenthumbcollective215@yahoo.com>
2019-09-07 01:47:18 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,17 @@ end
 
 def introduction_with_language_optional(name,language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  puts "Hi my name is {name}"
+end
+
+def introduction_with_language(name,language)
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+>>>>>>> dfe6b0d794188e933ff8ab880ebc05edff3a45e5
 end
 
 
