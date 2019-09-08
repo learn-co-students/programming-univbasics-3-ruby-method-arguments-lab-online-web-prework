35f5558eaf8b75c3cfa2520d285c8ef39f2a3cf6
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-08 17:55:36 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
-def #introduction(name)
+def introduction(name)
 puts "Hi, my name is #{name}."
 end
 
-def #introduction_with_language(name, language)
+def introduction_with_language(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def #introduction_with_language_optional(name, language="Ruby")
-puts "Hi, my name is #{name} annd I am learning to program in #{language}."
+def introduction_with_language_optional(name, language="Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
