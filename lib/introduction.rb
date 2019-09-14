b610a4d20be2270f598b813875197e3b32497e87
Derric Fragosa <fderric@gmail.com>
2019-09-14 03:34:26 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,17 +6,17 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction("Dan")
+
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end  
-  introduction_with_language("Dan","Ember.js")
+  
   
 def introduction_with_language_optional(name,language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
 end 
-introduction_with_language_optional("Dan")
+
 
   
 
