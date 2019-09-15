bc0e13a1bb0670f8b0fadebb45fa88f2e0dad5fd
Riley Moore <rilemoore@gmail.com>
2019-09-15 23:05:39 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,16 +5,26 @@
 
 
 
+name = "Riley"
+language = "C++"
 
+def introduction(name)
 
+  puts "Hi, my name is #{name}."
 
+end
 
+def introduction_with_language(name, language)
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+end  
 
-
-
-
-
-
+def introduction_with_language_optional(name, language = "Ruby")
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+end  
 
 
 
