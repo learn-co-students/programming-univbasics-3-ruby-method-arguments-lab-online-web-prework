127b36cc3d3d4c2eb5a5cd6e8e1d678f64474b83
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-09-17 15:21:27 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,4 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+Def "Hi, my name is #{Merkeresseria}."
 
 
 
