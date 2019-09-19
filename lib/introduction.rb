a201eec6a274c740bdf728bb3831151c336b1fcf
Ricky dildy <rdildy92@gmail.com>
2019-09-19 22:34:24 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,13 +2,19 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+name = "Ricky"
+language = "Ruby"
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
+def introduction_with_language(name, language)
+  puts "Hi, name is #{name} and I am learning to program in #{language}."
+end 
 
-
-
-
-
-
+def introduction_with_language_optional(name, language)
+  puts "Hi, name is #{name} and I am learning to program in #{language}."
+end   
 
 
 
