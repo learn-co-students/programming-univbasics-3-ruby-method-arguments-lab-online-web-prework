685c8215e58cb30d30e2d286b439206308af471c
William <teststuffreally@gmail.com>
2019-09-22 01:09:59 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,13 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+
+
 
 
 
