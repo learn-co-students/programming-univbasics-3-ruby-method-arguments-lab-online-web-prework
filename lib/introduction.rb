349ce72eeb076c3d5d2fa6a0779ae5be447d0fa9
Alex Martinez <alexandr3martin3z@gmail.com>
2019-09-22 03:27:55 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,25 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+def introduction(josh)
+ puts "Hi, my name is #{josh}."
+end
+
+def introduction_with_language(josh, react)
+  puts "Hi, my name is #{josh} and I am learning to program in #{react}."
+end
+
+def introduction_with_language_optional(edwin, language="Ruby")
+  puts "Hi, my name is #{edwin} and I am learning to program in #{language}."
+end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
