4075e0485d12147c292a4164acb3ba0877df41c4
Prince Charles <princecharles35@yahoo.com>
2019-09-23 10:39:52 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,9 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-
+name 'hi, my name is #{prince}'
+name_with_language 'hi my name is #{prince} and im here to learn #{programing}'
+name_with_languag_optional 'hi my name is #{prince} and im here to learn #{RUBY}'
 
 
 
