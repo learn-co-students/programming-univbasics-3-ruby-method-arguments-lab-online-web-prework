54e53442216f1206e7d8a49e3b80130ed55b5703
Makeda P Mckune <makedamckune@gmail.com>
2019-09-29 22:24:16 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,10 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
-
-
-
-
+puts "Enter Name"
+name = gets 
+puts ("Hello there, Welcome"+name)
+puts "Enter Name and Language"
+name = gets
+language = gets
+puts ("Hello I am"+name+"And I am learning to program in"+language)
 
 
 
