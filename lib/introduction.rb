09c6804345b58735b20661aed1162454842833bb
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-09-30 08:31:35 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,14 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+def introduction(Alianah)
+  puts "Hi, my name is {Alianah}."
+end
 
+def introduction_with_language(Alianah, language)
+  puts "Hi, my name is {Alianah} and I am learning to program in {Language}."
+end
 
-
-
-
-
+def introduction_with_language_optional(Alianah, language="Ruby")
+  puts "Hi, my name is {Alianah} and I am learning to program in {Language}."
+end
 
 
 
