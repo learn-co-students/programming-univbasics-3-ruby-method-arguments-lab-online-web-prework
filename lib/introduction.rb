24eae794825e12cb534f905e9a87a3cc53f4514d
samantha gustafson <sgustafson9612@gmail.com>
2019-10-01 02:15:42 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,15 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+def 
+#introduction
+"hi, my name is #name"
+end
+def
+#introduction_with_language
+"hi, my name is #{name} and i am learning to program #{ruby}"
+end
+def
+#introductiohn_with_language_optional
+"hi,my name is #{sam} and i am learning to program #{ruby}"
+end
 
 
 
