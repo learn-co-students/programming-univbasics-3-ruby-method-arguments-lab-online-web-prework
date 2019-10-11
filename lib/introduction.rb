4f8c811ba76f749d06a608468174d2198a0a799e
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-11 19:49:17 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,13 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning #{language}"
+end
+def introduction_with_language_optional(name, language = "Ruby")
 
 
 
