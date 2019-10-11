a306143dde54207be13a9cc28e4d50095549550d
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-11 19:51:37 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,7 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning #{language}"
 end
 def introduction_with_language_optional(name, language = "Ruby")
+end
 
 
 
@@ -90,14 +91,14 @@ def introduction_with_language_optional(name, language = "Ruby")
 
 
 
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
+def introduction(name)
+ puts "Hi, my name is #{name}."
+end
 
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
