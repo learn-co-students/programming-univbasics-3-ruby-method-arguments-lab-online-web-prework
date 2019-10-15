0c0cc95b823e2eeaddd39658e6aee0ab4dbbfe63
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-15 15:20:42 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,11 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-
+#introduction
+def introduction("josh") + ("Hi my name is josh")
+  #introduction_with_language
+#introduction_with_language_optional
+#introduction_with_language_optional
 
 
 
