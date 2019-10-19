197750d9abcfbe42676a87e4707bfc6c0213f669
Andrew H <andrew.o.hibbard@gmail.com>
2019-10-19 14:06:12 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,6 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language="Ruby")
-  if language == nil
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
 
