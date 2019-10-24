84b6ff2cd28512f2c27c8af473e6251052670064
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-24 17:34:01 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,19 +3,24 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def introduction(name = "Hi, my name is #{name}.")
+  puts "Hi, my name is #{name}."
+end
+
+introduction
+
+def introduction_with_language(name, language = "computer")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+introduction_with_language("Anna", "Ruby")
 
 
+def introduction_with_language_optional(name, language = "Ruby")
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
-
-
-
-
-
-
-
-
-
-
+introduction_with_language_optional("Leon")
 
 
 
