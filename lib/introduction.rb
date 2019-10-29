df66dc747c2554f8ee75232538b2b8cbc4b65014
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-10-29 03:50:49 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,14 +2,10 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-name="Markita"
-def introduction(name)
-  puts "Hi, my name is #{name}"
 
-
-
-
-
+def introduction_with_language(Markita, Ruby)
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
 
 
 
