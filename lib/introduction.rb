36990e45028bdf2764025e81c8d33142f4fe4774
Carly Larko <carly.larko@gmail.com>
2019-10-29 16:30:59 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,12 +3,15 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+#def introduction (name)
+ #puts "Hi, my name is #{name}"
+#end
 
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
-
-
-
-
+introduction_with_language ("Carly", "Ruby")
 
 
 
