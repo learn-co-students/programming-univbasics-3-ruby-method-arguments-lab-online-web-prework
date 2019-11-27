08fd4df8e032ecbc5a8eda86982cc13b0be5dec5
Yasmine Amin <yaz410@gmail.com>
2019-11-27 13:04:01 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,20 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+def introduction (Yasmine)
+puts "Hi, my name is #{Yasmine}."
+end
+
+# def introduction(Yasmine)
+#  puts "Hi, my name is #{Yasmine}."
+#end
+
+#def introduction_with_language(Yasmine, Ruby)
+#  puts "Hi, my name is #{Yasmine} and I am learning to program in #{ruby}."
+#end
+
+#def introduction_with_language_optional(name, language="Ruby")
+#  puts "Hi, my name is #{Yasmine} and I am learning to program in #{ruby}."
+#end
+
+
 
 
 
