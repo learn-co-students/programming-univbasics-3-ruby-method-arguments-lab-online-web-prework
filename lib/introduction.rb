8e4dfac8210a5d46bd5d26db85214fa8be5c8b48
Peter Bedrosian <peter_bedrosian@yahoo.com>
2019-12-31 18:32:17 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,8 +15,6 @@ def introduction_with_language_optional(name , language = 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("Peter")
-
 
 
 
