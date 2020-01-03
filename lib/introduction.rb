e10f86af87c8dceb9c998fb7f27f5dbedb6b990b
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-03 02:43:07 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,17 @@
 
 
 
+def introduction = (name)
+  "Hi my name is #{name}."
+end
 
+def introduction_with_language = (name,language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional = (name,language = "Ruby") 
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
